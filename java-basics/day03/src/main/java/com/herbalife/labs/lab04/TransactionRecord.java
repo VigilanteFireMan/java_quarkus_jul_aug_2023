fc4a7f610a2a6d32c6e96a2e7cdef155b5591977
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-07-20 07:48:00 +0530

    Day03

--- java-basics/day03/src/main/java/com/herbalife/labs/lab04/TransactionRecord.java
+++ java-basics/day03/src/main/java/com/herbalife/labs/lab04/TransactionRecord.java
@@ -3,13 +3,4 @@ package com.herbalife.labs.lab04;
 import java.time.LocalDateTime;
 
 public record TransactionRecord(int accountNumber, double amount, String transactionType, LocalDateTime localDateTime) {
-    @Override
-    public String toString() {
-        return "TransactionRecord{" +
-                "accountNumber=" + accountNumber +
-                ", amount=" + amount +
-                ", transactionType='" + transactionType + '\'' +
-                ", localDateTime=" + localDateTime +
-                '}';
-    }
 }
