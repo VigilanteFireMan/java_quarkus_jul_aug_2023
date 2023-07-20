a5f2d6b9215a80f3f8ffacafbabd69a5c2332dfc
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-07-20 08:02:24 +0530

    Day03

---
+++ java-basics/day03/src/main/java/com/herbalife/examples/InterfacesExample.java
@@ -0,0 +1,50 @@
+package com.herbalife.examples;
+
+public class InterfacesExample {
+    public static void main(String[] args) {
+        Shape square = new Square() {};
+        Figure square2 = new Square() {};
+        Printer printer = new PrinterImpl();
+
+        printer.print("some data");
+        printer.printInUpperCase("some data");
+        Printer.doSomething();
+    }
+}
+
+interface Printer {
+    void print(String data);
+    default void printInUpperCase(String data) {
+        System.out.println(data.toUpperCase());
+    }
+    static void doSomething() {
+
+    }
+}
+
+class PrinterImpl implements Printer {
+    public void print(String data) {
+        System.out.println(data);
+    }
+}
+
+
+interface Shape {
+    int area();
+}
+
+interface Figure {
+    void draw();
+}
+
+class Square implements Shape, Figure {
+    @Override
+    public int area() {
+        return -1;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
