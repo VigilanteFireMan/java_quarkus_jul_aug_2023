73507c88a4060d6e3321e2b78ffe6cb71f8e723c
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-07-27 09:44:59 +0530

    Day07

---
+++ java-basics/day07-concurrency/src/main/java/com/herbalife/examples/ParallelStreams.java
@@ -0,0 +1,14 @@
+package com.herbalife.examples;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ParallelStreams {
+    public static void main(String[] args) {
+        IntStream numbersStream = IntStream.range(1, 1000000);
+        numbersStream
+                .parallel() //by default creates a forkjoinpool with number of threads = (number of cores - 1)
+                .filter(e -> e % 2 == 0)
+                .forEach(e -> System.out.println(e + " on " + MyThreadUtil.name()));
+    }
+}
--- java-basics/day07-concurrency/src/main/java/com/herbalife/examples/ThreadPoolsInConcurrentPackage.java
+++ java-basics/day07-concurrency/src/main/java/com/herbalife/examples/ThreadPoolsInConcurrentPackage.java
@@ -2,6 +2,7 @@ package com.herbalife.examples;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 
 public class ThreadPoolsInConcurrentPackage {
     public static void main(String[] args) {
@@ -12,34 +13,45 @@ public class ThreadPoolsInConcurrentPackage {
         //ExecutorService executorService = Executors.newFixedThreadPool(3);
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        executorService.submit(() -> {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        System.out.println("FJP size: " + forkJoinPool.getPoolSize());
+        forkJoinPool.submit(() -> {
             while(true) {
-                System.out.println("Writing code on " + MyThreadUtil.name());
-                MyThreadUtil.sleep(2);
-            }
-        });
-
-        executorService.submit(() -> {
-            while(true) {
-                System.out.println("Searching on StOvFl on " + MyThreadUtil.name());
-                MyThreadUtil.sleep(10);
-            }
-        });
-
-        executorService.submit(() -> {
-            int count = 0;
-            while(count < 5) {
-                System.out.println("Scratching my head on " + MyThreadUtil.name());
+                System.out.println("Pushing changes to Bitbucket on " + MyThreadUtil.name());
                 MyThreadUtil.sleep(3);
-                count++;
             }
         });
-
-        executorService.submit(() -> {
-            while(true) {
-                System.out.println("Chatting with my colleague " + MyThreadUtil.name());
-                MyThreadUtil.sleep(4);
-            }
-        });
+        System.out.println("FJP size: " + forkJoinPool.getPoolSize());
+        MyThreadUtil.sleep(10); //ForkJoinPool exits if main thread ends. To avoid it hold on to the main thread for 10 seconds
+//
+//        executorService.submit(() -> {
+//            while(true) {
+//                System.out.println("Writing code on " + MyThreadUtil.name());
+//                MyThreadUtil.sleep(2);
+//            }
+//        });
+//
+//        executorService.submit(() -> {
+//            while(true) {
+//                System.out.println("Searching on StOvFl on " + MyThreadUtil.name());
+//                MyThreadUtil.sleep(10);
+//            }
+//        });
+//
+//        executorService.submit(() -> {
+//            int count = 0;
+//            while(count < 5) {
+//                System.out.println("Scratching my head on " + MyThreadUtil.name());
+//                MyThreadUtil.sleep(3);
+//                count++;
+//            }
+//        });
+//
+//        executorService.submit(() -> {
+//            while(true) {
+//                System.out.println("Chatting with my colleague " + MyThreadUtil.name());
+//                MyThreadUtil.sleep(4);
+//            }
+//        });
     }
 }
